52ddee59394449b54161f7bbe007c7d1e8a7c705
= <=>
2025-06-16 23:29:03 +0700

    up

--- Bai7/main.c
+++ Bai7/main.c
@@ -3,6 +3,7 @@
 int main(void) {
     int arr[]={2,3,1,12,8,56,34,23,12,8,9};
     int n = sizeof(arr) / sizeof(arr[0]);
+    //c1
     int found =0;
     for (int i = 0; i < n - 1; i++) {
         for (int j = i + 1; j < n; j++) {
@@ -16,6 +17,26 @@ int main(void) {
         printf("Mang co phan tu trung lap \n");
     }else
         printf("Mang khong co phan tu trung lap \n");
-    return 0;
     // Độ phức tạp thời gian: O(n²)
+
+
+    // c2
+    int count[1001] = {0};
+    found = 0;
+
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+        if (count[arr[i]] > 1) {
+            found = 1;
+            break;
+        }
+    }
+
+    if (found)
+        printf("Cach 2: Mang co phan tu trung lap\n");
+    else
+        printf("Cach 2: Mang khong co phan tu trung lap\n");
+    // độ phức tạp không gian O(1)
+    return 0;
+
 }
